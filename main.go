43b5f39a718d26bb4357a2796c0cda8567b32885
Ubuntu <noma@dasasaki-vm-discord-sktrythm.5larruiijw3eddb12dr4fbwtdc.lx.internal.cloudapp.net>
2021-09-17 17:42:58 +0000

    発言者のvoice channelに参加する

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"github.com/bwmarrin/discordgo"
 	"io/ioutil"
@@ -59,7 +60,33 @@ func msgReceived(s *discordgo.Session, m *discordgo.MessageCreate) {
 		nickname = member.Nick
 	}
 	fmt.Println(m.Content + " by " + nickname)
+	//fmt.Printf("(%%#v) %#v\n", m.Author)
+
+	// Find a user's current voice channel
+	vs, err := findUserVoiceState(s, m.Author.ID)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// join the voice channel the user is on
+	_, err = s.ChannelVoiceJoin(m.GuildID, vs.ChannelID, false, true)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
 	s.ChannelMessageSend(m.ChannelID, m.Content)
 	fmt.Println("送信したメッセージ:", m.Content)
+}
+
+func findUserVoiceState(session *discordgo.Session, userid string) (*discordgo.VoiceState, error) {
+	for _, guild := range session.State.Guilds {
+		for _, vs := range guild.VoiceStates {
+			if vs.UserID == userid {
+				return vs, nil
+			}
+		}
+	}
+	return nil, errors.New("Could not find user's voice state")
 }
