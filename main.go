62069ce300c80a3eced2d9df0ebb5ec521fc3c13
Ubuntu <noma@dasasaki-vm-discord-sktrythm.5larruiijw3eddb12dr4fbwtdc.lx.internal.cloudapp.net>
2021-09-18 06:55:10 +0000

    !sp [URLまたは検索キーワードで呼び出す様に変更

--- main.go
+++ main.go
@@ -10,11 +10,12 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
-	"os/signal"
 	"os/exec"
+	"os/signal"
+	"regexp"
+	"strings"
 	"syscall"
 	"time"
-	"strings"
 )
 
 // 構造体定義
@@ -63,82 +64,87 @@ func msgReceived(s *discordgo.Session, m *discordgo.MessageCreate) {
 		return
 	}
 
-	s.ChannelMessageSend(m.ChannelID, "再生を試みるんゴ")
+	// start play
+	if regexp.MustCompile(`^!sp `).Match([]byte(m.Content)) {
+		musicInfo := strings.Replace(m.Content, "!sp ", "", 1)
+		s.ChannelMessageSend(m.ChannelID, "再生を試みるんゴ")
 
-	nickname := m.Author.Username
-	member, err := s.State.Member(m.GuildID, m.Author.ID)
+		nickname := m.Author.Username
+		member, err := s.State.Member(m.GuildID, m.Author.ID)
 
-	if err == nil && member.Nick != "" {
-		nickname = member.Nick
-	}
-	fmt.Println(m.Content + " by " + nickname)
-	//fmt.Printf("(%%#v) %#v\n", m.ChannelID)
+		if err == nil && member.Nick != "" {
+			nickname = member.Nick
+		}
+		fmt.Println(m.Content + " by " + nickname)
+		//fmt.Printf("(%%#v) %#v\n", m.ChannelID)
 
-	// Find a user's current voice channel
-	vs, err := findUserVoiceState(s, m.Author.ID)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	// join the voice channel the user is on
-	vc, err := s.ChannelVoiceJoin(m.GuildID, vs.ChannelID, false, true)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	// dl youtube audio
-	cmdstr := "youtube-dl 'ytsearch:"
-	cmdstr += m.Content // movie name
-	cmdstr += "' -x --audio-format mp3 -o './tmp/%(playlist_index)s - %(title)s.%(ext)s' | grep mp3 | sed -e \"s/.*: //g\""
-	out, err := exec.Command("sh", "-c", cmdstr).Output()
-	if err != nil {
-		println("exec.Commandに失敗")
-	        s.ChannelMessageSend(m.ChannelID, "曲のDLに失敗したんゴ")
-		return
-	}
-	audioFilePath := strings.TrimRight(string(out),"\n")
-
-	// play audio file
-	opts := dca.StdEncodeOptions
-	opts.RawOutput = true
-	opts.Bitrate = 120
-	opts.Volume = 1
-
-	encodeSession, err := dca.EncodeFile(audioFilePath, opts)
-	if err != nil {
-		log.Fatal("Failed creating an encoding session: ", err)
-	        s.ChannelMessageSend(m.ChannelID, "エンコードに失敗したんゴ")
-		return
-	}
-
-	s.ChannelMessageSend(m.ChannelID, strings.TrimRight(string(out),"\n"))
-
-
-	done := make(chan error)
-	stream := dca.NewStream(encodeSession, vc, done)
-
-	ticker := time.NewTicker(time.Second)
-
-	for {
-		select {
-		case err := <-done:
-			if err != nil && err != io.EOF {
-				log.Fatal("An error occured", err)
-	                        s.ChannelMessageSend(m.ChannelID, "よくわからん処理に失敗したんゴ、すまんゴ")
-				return
-			}
-
-			// Clean up incase something happened and ffmpeg is still running
-			encodeSession.Truncate()
-			s.ChannelMessageSend(m.ChannelID, "再生したから落ちるんゴ、と思ったけど抜け方わからないんゴ")
+		// Find a user's current voice channel
+		vs, err := findUserVoiceState(s, m.Author.ID)
+		if err != nil {
+			fmt.Println(err)
 			return
-		case <-ticker.C:
-			stats := encodeSession.Stats()
-			playbackPosition := stream.PlaybackPosition()
+		}
 
-			fmt.Printf("Playback: %10s, Transcode Stats: Time: %5s, Size: %5dkB, Bitrate: %6.2fkB, Speed: %5.1fx\r", playbackPosition, stats.Duration.String(), stats.Size, stats.Bitrate, stats.Speed)
+		// join the voice channel the user is on
+		vc, err := s.ChannelVoiceJoin(m.GuildID, vs.ChannelID, false, true)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		// dl youtube audio
+		cmdstr := "youtube-dl 'ytsearch:"
+		cmdstr += musicInfo // movie name
+		cmdstr += "' -x --audio-format mp3 -o './tmp/%(title)s.%(ext)s' | grep mp3 | sed -e \"s/.*: //g\""
+		out, err := exec.Command("sh", "-c", cmdstr).Output()
+		if err != nil {
+			println("exec.Commandに失敗")
+			s.ChannelMessageSend(m.ChannelID, "曲のDLに失敗したんゴ")
+			return
+		}
+
+		audioFilePath := strings.TrimRight(string(out), "\n")
+		movieName := strings.Replace(strings.Replace(audioFilePath, "./tmp/", "", 1), ".mp3", "", 1)
+
+		// play audio file
+		opts := dca.StdEncodeOptions
+		opts.RawOutput = true
+		opts.Bitrate = 120
+		opts.Volume = 1
+
+		encodeSession, err := dca.EncodeFile(audioFilePath, opts)
+		if err != nil {
+			log.Fatal("Failed creating an encoding session: ", err)
+			s.ChannelMessageSend(m.ChannelID, "エンコードに失敗したんゴ")
+			return
+		}
+
+		s.ChannelMessageSend(m.ChannelID, "play -> "+movieName)
+
+		done := make(chan error)
+		stream := dca.NewStream(encodeSession, vc, done)
+
+		ticker := time.NewTicker(time.Second)
+
+		for {
+			select {
+			case err := <-done:
+				if err != nil && err != io.EOF {
+					log.Fatal("An error occured", err)
+					s.ChannelMessageSend(m.ChannelID, "よくわからん処理に失敗したんゴ、すまんゴ")
+					return
+				}
+
+				// Clean up incase something happened and ffmpeg is still running
+				encodeSession.Truncate()
+				s.ChannelMessageSend(m.ChannelID, "再生したから落ちるんゴ、と思ったけど抜け方わからないんゴ")
+				return
+			case <-ticker.C:
+				stats := encodeSession.Stats()
+				playbackPosition := stream.PlaybackPosition()
+
+				fmt.Printf("Playback: %10s, Transcode Stats: Time: %5s, Size: %5dkB, Bitrate: %6.2fkB, Speed: %5.1fx\r", playbackPosition, stats.Duration.String(), stats.Size, stats.Bitrate, stats.Speed)
+			}
 		}
 	}
 }
